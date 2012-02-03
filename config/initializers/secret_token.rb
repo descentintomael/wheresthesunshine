d188478fc0a6581e2dd5dcce5ea1e4fdf173c2df
Sean Todd <iphone.receive@gmail.com>
2012-02-03 15:55:49 -0800

    Initial commit.  Generated Rails structure only.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheresthesunshine::Application.config.secret_token = '9bef1324dab60ee6bc2b210b9f100d7f1b6891d1775ecf6622d1a20b3097e246503ec456a4534813531f71bc0597a0dfbf60623e21cc13dd1a3a4ac2016a6cf7'
