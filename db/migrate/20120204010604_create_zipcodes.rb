a17dcebcc6c574497eb822715c7f32ea2d5f8654
Sean Todd <iphone.receive@gmail.com>
2012-02-03 17:20:37 -0800

    Made the initial model framework

---
+++ db/migrate/20120204010604_create_zipcodes.rb
@@ -0,0 +1,13 @@
+class CreateZipcodes < ActiveRecord::Migration
+  def change
+    create_table :zipcodes do |t|
+      t.string :zipcode
+      t.float :lat
+      t.float :lng
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
