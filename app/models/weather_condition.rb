28e19d4ab17978447ead9a991ba96b7af45ee304
Sean Todd <iphone.receive@gmail.com>
2012-02-06 16:04:01 -0800

    Built the weather condition model and spec tests for it.

--- app/models/weather_condition.rb
+++ app/models/weather_condition.rb
@@ -1,3 +1,13 @@
 class WeatherCondition < ActiveRecord::Base
   belongs_to :zipcode
+  
+  validates :condition,   :presence => true
+  validates :temperature, :presence => true,
+                          :numericality => {  :only_integer => true, 
+                                              :less_than => 140 }
+  validates :wind_speed,  :presence => true,
+                          :numericality => {  :only_integer => true, 
+                                              :less_than => 250, 
+                                              :greater_than_or_equal_to => 0 }
+
 end
--- spec/models/weather_condition_spec.rb
+++ spec/models/weather_condition_spec.rb
@@ -6,30 +6,63 @@ describe WeatherCondition do
   before(:each) do
     # Any valid zipcode will do
     zipcode = Zipcode.first
-    @attr = { :zipcode=>zipcode,
-              :temperature=>86,
-              :wind_speed=>5,
-              :condition=>'Sunny'}
+    @attr = { :zipcode => zipcode,
+              :temperature => 86,
+              :wind_speed => 5,
+              :condition => 'Sunny'}
   end
   
-  it "should create a new instance given valid attributes" do 
+  it "should create a new instance given valid attributes" do
     WeatherCondition.create!(@attr)
   end
   
   describe "condition validation" do
-    
+    it "should require a weather condition" do
+      no_condition = WeatherCondition.create(@attr.merge(:condition => ''))
+      no_condition.should_not be_valid
+    end
   end
   
   describe "temperature validation" do
+    it "should require a temperature" do
+      no_temp = WeatherCondition.create(@attr.merge(:temperature => ''))
+      no_temp.should_not be_valid
+    end
     
+    it "should require an integer for temperature" do
+      float_temp = WeatherCondition.create(@attr.merge(:temperature => '10.1'))
+      float_temp.should_not be_valid
+    end
+    
+    it "should reject non-numeric values for temperature" do
+      non_numeric_temp = WeatherCondition.create(@attr.merge(:temperature => 'aa'))
+      non_numeric_temp.should_not be_valid
+    end
+    
+    it "should require an integer less than 140 for temperature" do
+      high_temp = WeatherCondition.create(@attr.merge(:temperature => '141'))
+      high_temp.should_not be_valid
+    end
   end
   
   describe "wind speed validation" do
+    it "should require a positive wind speed" do
+      negative_wind_speed = WeatherCondition.create(@attr.merge(:wind_speed => -1))
+      negative_wind_speed.should_not be_valid
+    end
     
+    it "should require an integer for wind speed" do
+      float_wind_speed = WeatherCondition.create(@attr.merge(:wind_speed => 10.1))
+      float_wind_speed.should_not be_valid
+    end
   end
-  
-  describe "zipcode validation" do
-    
-  end
+
+  # Commenting out for now until I figure out how to validate the relationship
+  # describe "zipcode validation" do
+  #   it "should require a zipcode" do
+  #     no_zip = WeatherCondition.create(@attr.merge(:zipcode => nil))
+  #     no_zip.should_not be_valid
+  #   end
+  # end
   
 end
