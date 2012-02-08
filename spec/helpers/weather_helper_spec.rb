7ff1f845674d7f7e5164ea4b01efcba56ad3e79c
Sean Todd <iphone.receive@gmail.com>
2012-02-08 09:41:31 -0800

    Stubs created by RSpec for whenever I manage to create the tests for my controllers and views.

---
+++ spec/helpers/weather_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the WeatherHelper. For example:
+#
+# describe WeatherHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe WeatherHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
