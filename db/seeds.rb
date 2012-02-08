a6c114829a67f7ce5d9d92664bdcb589cfd0c707
Sean Todd <iphone.receive@gmail.com>
2012-02-08 09:37:43 -0800

    Changed the zip code seeds csv to include the weather station.  Added a db seed script to push all of the data in that CSV up to the db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Loading up zipcode database"
+# Delete all the current zipcode records
+Zipcode.delete_all
+File.open("db/seeds/zipcode_seeds.csv", "r") do |zipcodes| 
+  zipcodes.read.each_line do |zipcode_line|
+    puts "Working with line: #{zipcode_line.chomp.split(',')}"
+    zipcode,population,city,state,lat,lng,weather_station = zipcode_line.chomp.split(',')
+    Zipcode.create!(:zipcode => zipcode,
+                    :lat => lat,
+                    :lng => lng,
+                    :zipcode_point => Point.from_x_y(lat.to_f,lng.to_f),
+                    :city => city,
+                    :state => state,
+                    :population => population,
+                    :weather_station => weather_station)
+  end
+end
+puts "Finished loading zipcode database"
+puts "Be sure to load up the current weather conditions"
+
