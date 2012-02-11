dd7bba3e3caff87054bc101d09e967a785af15cc
Sean Todd <iphone.receive@gmail.com>
2012-02-11 15:57:40 -0800

    Modified the spec file to use the shoulda framework instead

--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -9,85 +9,38 @@ describe Zipcode do
               :city=>'Nowhere',
               :state=>'CA',
               :population=>90000}
-  end
-  
-  it "should create a new instance given valid attributes" do 
     Zipcode.create!(@attr)
   end
   
-  describe "zipcode validations" do
-    it "should require a zipcode" do
-      missing_zipcode = Zipcode.new(@attr.merge(:zipcode=>""))
-      missing_zipcode.should_not be_valid
-    end
-    
-    # Note, these two tests may disappear later on if/when Canada is included
-    it "should only accept digits in the zipcode" do
-      invalid_zipcode = Zipcode.new(@attr.merge(:zipcode=>"nozip"))
-      invalid_zipcode.should_not be_valid
-    end
-    
-    it "shouldn't allow more than 5 digits" do
-      long_zipcode = Zipcode.new(@attr.merge(:zipcode=>"12345678"))
-      long_zipcode.should_not be_valid
-    end
-    
-    it "shouldn't allow less than 5 digits" do
-      short_zipcode = Zipcode.new(@attr.merge(:zipcode=>"1234"))
-      short_zipcode.should_not be_valid
-    end
-    
-    it "shouldn't allow duplicate zipcodes" do
-      existing_zipcode = Zipcode.first
-      dupe_zipcode = Zipcode.new(@attr.merge(:zipcode=>existing_zipcode[:zipcode]))
-      dupe_zipcode.should_not be_valid
-    end
+  after(:each) do
+    Zipcode.delete_all
   end
   
-  describe "state validations" do
-    it "should require a state" do
-      missing_state = Zipcode.new(@attr.merge(:state=>""))
-      missing_state.should_not be_valid
-    end
-    
-    it "should only accept letters" do
-      invalid_state = Zipcode.new(@attr.merge(:state=>"12"))
-      invalid_state.should_not be_valid
-    end
-    
-    it "shouldn't allow more than two characters" do
-      long_state = Zipcode.new(@attr.merge(:state=>"california"))
-      long_state.should_not be_valid
-    end
-    
-    it "shouldn't allow less than two characters" do
-      short_state = Zipcode.new(@attr.merge(:state=>"c"))
-      short_state.should_not be_valid
-    end
+  it { should have_many(:weather_conditions) }
+  
+  # Zipcode field validations
+  it { should validate_presence_of(:zipcode) }
+  it { should validate_numericality_of(:zipcode) }
+  it { should ensure_length_of(:zipcode).is_equal_to(5) }
+  it { should validate_uniqueness_of(:zipcode) }
+
+  # State field validations
+  it { should validate_presence_of(:state) }
+  it { should_not allow_value("12").for(:state) }
+  it { should ensure_length_of(:state).is_equal_to(2) }
+  
+  # Geolocation validations
+  it { should validate_presence_of(:lat) }
+  it { should validate_presence_of(:lng) }
+  it "should have the same X value of the point as the latitude" do
+    Zipcode.first.zipcode_point.x.should == Zipcode.first.lat
   end
   
-  describe "geolocation validations" do
-    it "should require a latitude" do
-      missing_lat = Zipcode.new(@attr.merge(:lat=>nil))
-      missing_lat.should_not be_valid
-    end
-    
-    it "should require a longitude" do
-      missing_lng = Zipcode.new(@attr.merge(:lng=>nil))
-      missing_lng.should_not be_valid
-    end
+  it "should have the same Y value of the point as the longitude" do
+    Zipcode.first.zipcode_point.y.should == Zipcode.first.lng
   end
   
-  describe "city validations" do
-    it "should require a city" do
-      missing_city = Zipcode.new(@attr.merge(:city=>""))
-      missing_city.should_not be_valid
-    end
-    
-    it "shouldn't allow long city names" do
-      long_city = "a"*100
-      long_city_zipcode = Zipcode.new(@attr.merge(:city=>long_city))
-      long_city_zipcode.should_not be_valid
-    end
-  end
+  # City field validations
+  it { should validate_presence_of(:city) }
+  it { should ensure_length_of(:city).is_at_most(60) }
 end
